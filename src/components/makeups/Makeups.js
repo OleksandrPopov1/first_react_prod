c5cac44c5dab6b5bd7fb0cafe936ee2ae38c301f
Rams-Popov <100295237+Rams-Popov@users.noreply.github.com>
2022-06-28 21:10:10 +0300

    hw3 add

--- src/components/makeups/Makeups.js
+++ src/components/makeups/Makeups.js
@@ -1,9 +1,17 @@
+import {useEffect, useState} from "react";
+import {getMakeups} from "../../services";
+
 function Makeups(){
-    
+
+    const [makeups, setMakeups] = useState([]);
+
+    useEffect(() => {
+        getMakeups().then(({data}) => setMakeups([...data]));
+    }, []);
 
     return (
         <div>
-
+            {makeups.map(makeup)}
         </div>
     );
 }
