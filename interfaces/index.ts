c7a74da3816e059dba8c9851fc6f1e47905ea99b
Rams-Popov <100295237+Rams-Popov@users.noreply.github.com>
2022-07-26 13:55:59 +0300

    hw7_TS

---
+++ interfaces/index.ts
@@ -0,0 +1,2 @@
+export * from './mission.interface';
+export * from './user.interface';
---
+++ types/functions.types.ts
@@ -0,0 +1,8 @@
+type sumType = (a:number, b:number) => number;
+
+type changeUserAgeType = (user:object, inc:number) => object;
+
+export type {
+    sumType,
+    changeUserAgeType
+};
